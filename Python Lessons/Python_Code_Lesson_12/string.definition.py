1fde097fa68e686b68910ad44122476930e9d8b1
Maras <84789663+Marasgeon@users.noreply.github.com>
2024-11-13 09:44:29 +0200

    Added Lesson 12.
    Added Lesson 12 Code.

---
+++ Python Lessons/Python_Code_Lesson_12/string.definition.py
@@ -0,0 +1,11 @@
+# Example 1: string.definition.py1
+empty_str = ""
+string = 'Hello World!'
+string2 = 'I said "Hello"!'
+mult_string = """...
+Hello there! 
+ I am a multi-line string! 
+"""
+print(string2)
+print(mult_string)
+print(type(string2))
---
+++ Python Lessons/Python_Code_Lesson_12/string.escape.characters.py
@@ -0,0 +1,6 @@
+# Example 3: string.escape.characters.py
+print("quotes: \", \'")
+print("slashes: / \\")
+print("tabs:\n\t|\t|a\t|aa\t|aaa\t|")
+print("change line: \n newline \r CR")
+print("Backspace: bb\baa\bc\b\baa\b")
