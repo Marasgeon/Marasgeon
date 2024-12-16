8686667fd72676bad2fc3ae97bfc26106c37e464
Maras <84789663+Marasgeon@users.noreply.github.com>
2024-12-16 09:49:00 +0200

    Completed Lesson 12.
    Completed Lesson 12 Python Code.

---
+++ Python Lessons/Python_Code_Lesson_12/string.operators.py
@@ -0,0 +1,17 @@
+# String operators example
+string = "Hello "
+concatenation = string + string
+print(f"Concatenation(+): {concatenation}")
+multiplication = string * 5
+print(f"Multiplication(*): {multiplication}")
+string += "World!" # string = string + "World!"
+print(f"Increment: {string}")
+print(f"Char in string: 'W' in {string}: {'W' in string}")
+print(f"Char not in string: 'W' not in {string}: {'W' not in string}")
+comparison = "abc" < "abd" # also: >, <=, >=
+print(f"Comparison: {comparison}")
+equality = "aa" == "AA".lower() # also not equal: !=
+print(f"Equality: {equality}")
+
+# Note: The operator += is a short form 
+# of the expression: string = string + "Word!"
---
+++ Python Lessons/Python_Code_Lesson_12/string.upper.lower.py
@@ -0,0 +1,8 @@
+# Example 7: string.upper.lower.py
+string = "Burning in Water, Drowning in Flame"
+print("capitalize: " + string.capitalize())
+print("lower: " + string.lower())
+print("casefold: " + string.casefold())
+print("upper: " + string.upper())
+print("title: " + string.title())
+print("swapcase: " + string.swapcase())
